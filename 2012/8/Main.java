71f048eb9b03282f230d857b56e721f5cc43e5dc
Jonathan Jimenez <jonathanjimenezromo@gmail.com>
2015-10-26 23:00:41 -0700

    Fixed solution to 2012/8 to not accept non prime factors

--- 2012/8/Main.java
+++ 2012/8/Main.java
@@ -1,20 +1,29 @@
 import java.util.Scanner;
+import java.lang.Math;
 class aout{
     public aout(String... args){
         Scanner s = new Scanner(System.in);
         long number, factor;
         nums: while(s.hasNext()){
             number = s.nextLong();
-            for(int i = 1; i <= (number / 4); i++){
+            for(int i = 1; i <= Math.sqrt(number); i++){
                 if(number % (4*i+1) == 0){
-                    System.out.println(number + " yes");
-                    System.out.println(i);
-                    continue nums;
+                    if(isPrime(4*i+1)){
+                        System.out.println(number + " yes");
+                        continue nums;
+                    }
                 }
             }
             System.out.println(number + " no");
         }
         s.close();
+    }
+    static boolean isPrime(int num){
+        for(int i = 2; i < (int)(0.5 + Math.sqrt(num)); i++){
+            if(num%i==0)
+                return false;
+        }
+        return true;
     }
     public static void main(String[] args){
         new aout();
