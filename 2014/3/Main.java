f84834817d0fd9772d63f0c0373314f6d28a97e3
Jonathan Jimenez <jonathanjimenezromo@gmail.com>
2015-10-30 15:33:40 -0700

    made aout private in problem 3 from 20014

--- 2014/3/Main.java
+++ 2014/3/Main.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 import java.math.*;
-public class aout{
+class aout{
 	public static void main(String[] args){
 		Scanner s = new Scanner(System.in);
 		int amount = s.nextInt();
